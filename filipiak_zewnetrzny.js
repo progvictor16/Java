08693cc1ce936fdf7ab221650838677ccb80bb24
progvictor16 <144625556+progvictor16@users.noreply.github.com>
2024-09-18 18:57:30 +0200

    Add files via upload

---
+++ filipiak_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("ostatnia modyfikacja strony".fontcolor("#00FF00  ").bold().fontsize(5)+"<br>"); 
+document.write(document.lastModified.fontcolor("#00FF00 ").bold()); 
---
+++ filipiak_zewnetrzny_zadanie_2.js
@@ -0,0 +1,2 @@
+document.write("Ostatnia modyfikacja strony".fontcolor("#00FF00  ").bold().fontsize(5)+"<br>"); 
+document.write(document.lastModified.fontcolor("#00FF00 ").bold()); 
